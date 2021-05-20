e8fb9d121852274df67bb02e2b7548f4ffa8a978
Adonys Santos <adonysmundotech@gmail.com>
2021-05-19 22:47:37 -0400

    feat: Add the listeners of the links to filter by category

--- js/index.js
+++ js/index.js
@@ -7,4 +7,19 @@ const grid = new Muuri(".grid", {
 window.addEventListener("load", () => {
   grid.refreshItems().layout();
   document.getElementById("grid").classList.add("loaded");
+
+  // Add the listeners of the links to filter by category
+  const links = document.querySelectorAll("#category a");
+  links.forEach((element) => {
+    element.addEventListener("click", (event) => {
+      const category = event.target.innerHTML.toLowerCase();
+
+      event.preventDefault();
+      links.forEach((link) => link.classList.remove("active"));
+      event.target.classList.add("active");
+      category === "all"
+        ? grid.filter(`[data-category]`)
+        : grid.filter(`[data-category="${category}]"`);
+    });
+  });
 });
