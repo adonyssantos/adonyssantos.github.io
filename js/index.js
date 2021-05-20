9cefc34770fbcd4ef3459f80b830c1b094147db8
Adonys Santos <adonysmundotech@gmail.com>
2021-05-19 23:46:03 -0400

    Add listener for images

--- js/index.js
+++ js/index.js
@@ -28,4 +28,24 @@ window.addEventListener("load", () => {
     const search = event.target.value;
     grid.filter((item) => item.getElement().dataset.tags.includes(search));
   });
+
+  // Add listener for images
+  const overlay = document.getElementById("overlay");
+  document.querySelectorAll(".grid .item img").forEach((element) => {
+    element.addEventListener("click", () => {
+      const path = element.getAttribute("src");
+      const title = element.parentNode.parentNode.dataset.title;
+      const description = element.parentNode.parentNode.dataset.description;
+      const project_repository =
+        element.parentNode.parentNode.dataset.project_repository;
+
+      overlay.classList.add("active");
+      document.querySelector("#overlay img").src = path;
+      document.querySelector("#overlay .title").innerHTML = title;
+      document.querySelector("#overlay .description").innerHTML = description;
+      document.querySelector("#overlay a").href = project_repository;
+      document.querySelector("#overlay a").innerHTML = project_repository;
+      
+    });
+  });
 });
