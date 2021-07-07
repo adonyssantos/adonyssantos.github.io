c809476ebb96d4d4e584216607f7485ef58c513d
Adonys Santos <adonysmundotech@gmail.com>
2021-07-07 00:48:02 -0400

    Add link handler

---
+++ assets/js/data.js
@@ -0,0 +1,6 @@
+const DATA = [
+  { title: 'Discord', link: 'https://discord.gg/fyGx7BZ' },
+  { title: 'Portfolio', link: './portfolio' },
+  { title: 'GitHub', link: 'https://github.com/adonyssantos' },
+  { title: 'LinkedIn', link: 'https://www.linkedin.com/in/adonyssantos' },
+];
---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+// DOM Printer
+const print = (id, data) => (document.getElementById(id).innerHTML = data);
+
+// Create HTML <ul> with links
+const createList = links => {
+  const createListElement = ({ title, link }) => {
+    return `
+		  <li>
+			  <a
+			  target="_blank"
+				  a href="${link}"
+				  rel="noopener noreferrer"
+				  class="link"
+				  >${title}</a
+			  >
+		  </li>
+		`;
+  };
+
+  let list = ``;
+  links.map(link => (list += createListElement({ ...link })));
+  list = `<ul>${list}</ul>`;
+  return list;
+};
+
+// Print the data link list when tha page is loaded
+window.addEventListener('load', () => {
+  print('links', createList(DATA));
+});
