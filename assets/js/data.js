d01b639eb57aca03441265ba6d41b782142aaed7
Adonys Santos <adonysmundotech@gmail.com>
2021-07-09 21:31:43 -0400

    Update data

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,6 +1,8 @@
 const DATA = [
-  { title: 'Discord', link: 'https://discord.gg/fyGx7BZ' },
   { title: 'Portfolio', link: './portfolio' },
   { title: 'GitHub', link: 'https://github.com/adonyssantos' },
   { title: 'LinkedIn', link: 'https://www.linkedin.com/in/adonyssantos' },
+  { title: 'Twitter', link: 'https://twitter.com/adonyssantos_' },
+  { title: 'Instagram', link: 'https://www.instagram.com/adonyssantos_' },
+  { title: "Discord's server", link: 'https://discord.gg/fyGx7BZ' },
 ];
