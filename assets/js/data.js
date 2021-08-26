e5c4918495e7a231f59ec0980d4bcfbee487de4d
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-08-26 11:52:29 -0400

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,8 +1,9 @@
 const DATA = [
-  { title: 'Portfolio', link: './portfolio' },
+  // { title: 'Portfolio', link: './portfolio' },
+  { title: 'Discord', link: 'https://discord.gg/fyGx7BZ' },
   { title: 'GitHub', link: 'https://github.com/adonyssantos' },
   { title: 'LinkedIn', link: 'https://www.linkedin.com/in/adonyssantos' },
   { title: 'Twitter', link: 'https://twitter.com/adonyssantos_' },
+  { title: 'TikTok', link: 'https://tiktok.com/@puzzlerdev' },
   { title: 'Instagram', link: 'https://www.instagram.com/adonyssantos_' },
-  { title: "Discord's server", link: 'https://discord.gg/fyGx7BZ' },
 ];
